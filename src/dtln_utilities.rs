b1376ad600466dfcc4bcaff0b086bd08b51f8969
David Négrier <d.negrier@thecodingmachine.com>
2026-03-12 16:29:25 +0100

    Optimize WASM denoise hot path

--- src/dtln_utilities.rs
+++ src/dtln_utilities.rs
@@ -1,10 +1,7 @@
 // FFI Wrappers and raw interfaces to DTLN engine.
-use once_cell::sync::Lazy;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::Result;
 use std::path::Path;
-use std::sync::Mutex;
 use wav::Header;
 
 use crate::dtln_engine::{dtln_create, dtln_denoise, DtlnEngine};
@@ -53,52 +50,51 @@ struct audio_buffer {
     data: [f32; WASM_AUDIO_BLOCK_SIZE],
 }
 
-static ENGINE_MAP: Lazy<Mutex<HashMap<u32, DtlnEngine>>> = Lazy::new(|| Mutex::new(HashMap::new()));
-static AUDIO_BUFFER_MAP: Lazy<Mutex<HashMap<u32, audio_buffer>>> =
-    Lazy::new(|| Mutex::new(HashMap::new()));
-static CURRENT_MAP_ID: Lazy<Mutex<u32>> = Lazy::new(|| Mutex::new(0));
+#[repr(C)]
+struct wasm_denoiser {
+    engine: DtlnEngine,
+    audio_buffer: audio_buffer,
+}
+
+fn wasm_denoiser_from_id(id: u32) -> &'static mut wasm_denoiser {
+    let ptr = id as usize as *mut wasm_denoiser;
+    assert!(!ptr.is_null(), "Engine not found for {}", id);
+    unsafe { &mut *ptr }
+}
 
 /**
  * Create a new DtlnEngine and return a unique id for it.
  */
 pub fn dtln_create_global() -> u32 {
-    let mut engine_map = ENGINE_MAP.lock().unwrap();
-    let mut memory_map = AUDIO_BUFFER_MAP.lock().unwrap();
-
     let engine = dtln_create();
-    let id = *CURRENT_MAP_ID.lock().unwrap();
-    *CURRENT_MAP_ID.lock().unwrap() += 1;
-
     let Some(engine) = engine else {
         panic!("Failed to create DtlnEngine");
     };
 
-    engine_map.insert(id, engine);
-    memory_map.insert(
-        id,
-        audio_buffer {
+    let handle = Box::new(wasm_denoiser {
+        engine,
+        audio_buffer: audio_buffer {
             data: [0.0; WASM_AUDIO_BLOCK_SIZE],
         },
-    );
-    id
+    });
+
+    let ptr = Box::into_raw(handle);
+    ptr as usize as u32
 }
 
 pub fn dtln_destroy_global(id: u32) {
-    let mut engine_map = ENGINE_MAP.lock().unwrap();
-    let mut memory_map = AUDIO_BUFFER_MAP.lock().unwrap();
+    let ptr = id as usize as *mut wasm_denoiser;
+    if ptr.is_null() {
+        return;
+    }
 
-    engine_map.remove(&id);
-    memory_map.remove(&id);
+    unsafe {
+        drop(Box::from_raw(ptr));
+    }
 }
 
 pub fn dtln_get_audio_buffer_raw_ptr(id: u32) -> *const f32 {
-    let mut memory_map = AUDIO_BUFFER_MAP.lock().unwrap();
-    let buffer = memory_map.get_mut(&id);
-    if buffer.is_none() {
-        panic!("Audio buffer not found for {}", id);
-    }
-    let buffer = buffer.unwrap();
-    buffer.data.as_ptr()
+    wasm_denoiser_from_id(id).audio_buffer.data.as_ptr()
 }
 
 /**
@@ -106,20 +102,10 @@ pub fn dtln_get_audio_buffer_raw_ptr(id: u32) -> *const f32 {
  * @param id The unique id of the engine to use.
  */
 pub fn dtln_denoise_global(id: u32) -> Result<()> {
-    let mut engine_map = ENGINE_MAP.lock().unwrap();
+    let denoiser = wasm_denoiser_from_id(id);
+    let input = denoiser.audio_buffer.data;
 
-    let engine = engine_map.get_mut(&id);
-    if engine.is_none() {
-        panic!("Engine not found for {}", id);
-    }
-    let engine = engine.unwrap();
-
-    let mut memory_map = AUDIO_BUFFER_MAP.lock().unwrap();
-    let audio_buffer = memory_map.get_mut(&id).unwrap();
-
-    let mut out = [0.0; WASM_AUDIO_BLOCK_SIZE];
-    if dtln_denoise(engine, &audio_buffer.data, &mut out).is_ok() {
-        audio_buffer.data.copy_from_slice(&out);
+    if dtln_denoise(&mut denoiser.engine, &input, &mut denoiser.audio_buffer.data).is_ok() {
         Ok(())
     } else {
         Err(std::io::Error::new(
